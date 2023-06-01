65eeacea97c878031d9456e8b0860d0ad8145150
JulioVianaDev <JulioVianaDev@gmail.com>
2023-06-01 15:15:55 -0300

    estado do cron funcionando

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const app = express();
 const port = 3000;
-app.use(cors())
+const LedRouter = require('./routes/ledRoutes')
+app.use(cors());
+
+app.use("/led",LedRouter)
+
 app.get('/api', (req, res) => {
-  const response = { message: "success",led: "ligado" };
+  const response = { message: 'success', led: true };
   res.json(response);
 });
 
--- server/routes/ledRoutes.js
+++ server/routes/ledRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const LedController = require('../controllers/LedController');
+
 router.get('/',LedController.init)
+router.get('/state-led',LedController.ledState)
 
 module.exports = router
